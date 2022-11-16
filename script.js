898de788f9892a3c15bff8748bf07b50e3c82e9c
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-11-16 15:15:52 +0100

    finished Plugin

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-const btn = document.querySelector("button[data-cy='showMore']");
-const jobs = Number(
-  document.querySelector(".JobSearchJobs_headline__Wj7Mk span").innerText
-);
-const h1 = document.querySelector(".JobSearchJobs_headline__Wj7Mk");
+setTimeout(() => {
+  let search = location.search;
 
-const delta = 1000;
-let jobCards = 0;
-let id;
-
-const on = (delta) => {
-  id = setInterval(() => {
-    console.info("Intervall Working..." + id);
-    btn.click();
-    // window.scrollTo(0, document.body.scrollHeight);
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      left: 0,
-      behavior: "smooth",
-    });
-    jobCards = document.querySelectorAll(
-      ".CardJob_jobCard__wBvr4[data-job-id]"
-    ).length;
-    if (jobCards === jobs) {
-      off(id);
+  const checkPath = () => {
+    if (location.search === search) {
+      return true;
     }
-  }, delta);
-};
+    return false;
+  };
 
-const off = (id) => {
-  clearInterval(id);
-  h1.insertAdjacentHTML(
-    "beforeend",
-    `<strong id='fastPlugin'>&nbsp; <span>${jobCards}</span> Jobs Gelistet bitte STRG + F nutzen</strong>`
+  const btn = document.querySelector("button[data-cy='showMore']");
+  let jobs = Number(
+    document.querySelector(".JobSearchJobs_headline__Wj7Mk span").innerText
   );
-  window.scroll({ top: 0, left: 0, behavior: "smooth" });
-  console.info("Intervall " + id + " shut down!");
-};
 
-window.addEventListener("keydown", (e) => {
-  if (e.isComposing || e.keyCode === 74) {
-    on();
-    console.warn("To cancel Press K ");
-    // alert("To cancel Press K ");
-  }
-  if (e.isComposing || e.keyCode === 75) {
-    off(id);
-  }
-});
+  const h1 = document.querySelector(".JobSearchJobs_headline__Wj7Mk");
+
+  const delta = 100;
+  let jobCards = 0;
+  let id;
+
+  const on = (delta) => {
+    id = setInterval(() => {
+      console.info("Intervall Working..." + id);
+      btn.click();
+      // window.scrollTo({top: document.body.scrollHeight,left: 0,behavior: "smooth",});
+      jobCards = document.querySelectorAll(
+        ".CardJob_jobCard__wBvr4[data-job-id]"
+      ).length;
+      if (jobCards === jobs) {
+        off(id);
+      }
+    }, delta);
+  };
+
+  const off = (id) => {
+    clearInterval(id);
+    removeLoader();
+    createInfoText();
+    // window.scroll({ top: 0, left: 0, behavior: "smooth" });
+    console.info("Intervall " + id + " shut down!");
+  };
+
+  const createInfoText = () => {
+    if (h1.childElementCount === 1) {
+      h1.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div id='fastPlugin'>
+          <span>${jobCards}</span> JobCards </br>
+          Bitte STRG + F nutzen </br>
+          <a id="reloadPage" href="javascript:location.reload(true)">reload page for new use!</a>
+        </div>
+        `
+        // only for dev
+        // <h2 id='dev'>Jobs: ${jobs}; JobCards: ${jobCards}</h2>
+      );
+    }
+    return;
+  };
+
+  const removeLoader = () => {
+    let loader = document.querySelector(".lds-dual-ring");
+    loader.remove();
+  };
+  const showLoader = () => {
+    h1.insertAdjacentHTML("beforeend", '<div class="lds-dual-ring"></div>');
+  };
+
+  window.addEventListener(
+    "keydown",
+    (e) => {
+      if (e.isComposing || e.keyCode === 74) {
+        on(delta);
+        showLoader();
+        console.warn("To cancel Press K ");
+        // alert("To cancel Press K ");
+      }
+      if (e.isComposing || e.keyCode === 75) {
+        off(id);
+      }
+    },
+    { once: true }
+  );
+}, 1000);
